8f3ecd17fd5e9ea18c4b6ec22dba14115746d397
adarsh-bhatnagar <adarshb.best@gmail.com>
2025-10-07 22:04:28 +0530

    fix: added cors origin

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,17 @@ import jobRoutes from "./routes/jobRoutes";
 // import applicationRoutes from "./routes/applicationRoutes";
 // import savedJobRoutes from "./routes/savedJobRoutes";
 
+const PORT = process.env.NEXT_API_URL || 4000;
+
 
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: ['http://localhost:3000', 'https://your-frontend.vercel.app'], // allowed origins
+    credentials: true, // if you're sending cookies or auth headers
+  }
+));
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
@@ -21,6 +28,6 @@ app.use("/api/v1/jobs", jobRoutes);
 // app.use("/api/applications", applicationRoutes);
 // app.use("/api/saved-jobs", savedJobRoutes);
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log("Server running on http://localhost:4000");
 });
